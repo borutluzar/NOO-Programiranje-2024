4b6bf3774e57b8e8111123a381fda7ea5bc7058c
Borut Lužar <borut.luzar@gmail.com>
2025-03-22 10:04:37 +0100

    Izpit 22. 3. 2025

---
+++ Izpit_2025-03-22/Naloga1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Izpit_2025_03_22
+{
+    /// <summary>
+    /// NALOGA 1:
+    /// 
+    /// Napišite funkcijo Zaokrozevanje, ki kot parameter prejme seznam decimalnih števil. 
+    /// Funkcija naj vsak število iz seznama zaokroži navzgor, 
+    /// če je njegov decimalni del večji od 0.4, sicer pa naj ga zaokroži navzdol. 
+    /// V ukazno vrstico naj nato funkcija izpiše število tistih števil, ki jih je zaokrožila navzgor, 
+    /// vrne pa naj povprečje zaokroženih vrednosti.                            [20 točk]
+    /// 
+    /// V funkciji ResitevNaloge pripravite seznam z vsaj desetimi števili. 
+    /// Pokličite funkcijo Zaokrozevanje in izpišite vrednost, ki jo funkcija vrne, 
+    /// v ukazno vrstico. V funkciji ResitevNaloge pripravite seznam z vsaj desetimi števili. 
+    /// Pokličite funkcijo Zaokrozevanje in izpišite vrednost, 
+    /// ki jo funkcija vrne, v ukazno vrstico.                                  [5 točk]
+    /// </summary>
+    public class Naloga1
+    {
+        /// <summary>
+        /// V tej funkciji se začne izvajati program za reševanje Naloge 1
+        /// </summary>
+        public static void ResitevNaloge()
+        {
+
+        }
+    }
+}
---
+++ Izpit_2025-03-22/Naloga2.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.ConstrainedExecution;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Izpit_2025_03_22
+{
+    /// <summary>
+    /// NALOGA 2:
+    /// 
+    /// Pripravljamo aplikacijo za samodejno generiranje izpitov. 
+    /// Zanjo bomo pripravili dva razreda: razred Naloga, ki bo predstavljal izpitno nalogo, 
+    /// in razred Izpit, ki bo predstavljal izpit iz programiranja. 
+    /// 
+    /// Razred Naloga naj vsebuje: 
+    /// - Lastnosti Besedilo, MozneTocke, DosezeneTocke, Avtor in Tezavnost.
+    /// - Natanko en konstruktor, ki nastavi lastnost Sestavljalec.
+    /// - Funkcijo, ki vrne odstotek doseženih točk.
+    /// - Metodo ToString, ki izpiše vse lastnosti naloge.                              [10 točk]
+    /// 
+    /// Razred Izpit naj vsebuje: 
+    /// - Lastnosti Predmet, DatumPisanja, Naloge in CasPisanja.
+    /// - Konstruktor, ki kot parameter dobi datum pisanja.
+    /// - Funkcijo ToString, ki naj izpiše vse podrobnosti izpita.
+    /// - Funkcijo, ki izračuna skupno število doseženih točk na izpitu.                [10 točk]
+    /// 
+    /// V funkciji ResitevNaloge kreirajte tri instance razreda Naloga 
+    /// in eno instanco razreda Izpit ter naloge dodajte k nalogam izpita. 
+    /// Vsem instancam za vse lastnosti določite neke vrednosti. 
+    /// Nato v ukazno vrstico izpišite število doseženih točk na izpitu.                [5 točk]
+    /// </summary>
+    public class Naloga2
+    {
+        /// <summary>
+        /// V tej funkciji se začne izvajati program za reševanje Naloge 2
+        /// </summary>
+        public static void ResitevNaloge()
+        {
+
+        }
+    }
+}
