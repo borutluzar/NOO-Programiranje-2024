81990dce18f281f099b4ddfb2a1fb4c69cbc0761
borutluzar <borut.luzar@gmail.com>
2025-04-01 19:58:31 +0200

    Naloga 4

--- Izpit_2025-03-22/Naloga4.cs
+++ Izpit_2025-03-22/Naloga4.cs
@@ -31,6 +31,63 @@ namespace Izpit_2025_03_22
             string filePath1 = "Viri/matrix.txt";
             string filePath2 = "Viri/matrixBig.txt";
 
-        }                
+            AnalizaZadnjih(filePath1, "naloga4.txt");
+            AnalizaZadnjih(filePath2, "naloga4Big.txt");
+        }
+
+        /// Pripravite metodo AnalizaZadnjih, ki kot parameter dobi 
+        /// pot do datoteke s tabelo števil. 
+        /// Prebere naj zadnje število v vsaki vrstici, 
+        /// poišče najmanjše med vsemi in ga prišteje prvemu številu vsake vrstice. 
+        /// Tako popravljeno tabelo zapišite v novo datoteko, 
+        /// katere ime prav tako prejmete kot parameter metode.                         [20 točk]
+        public static void AnalizaZadnjih(string potDoDatoteke, string novaDatoteka)
+        {
+            StreamReader srFile = new StreamReader(potDoDatoteke);
+
+            List<List<int>> lstMatrika = new List<List<int>>();
+
+            int min = int.MaxValue;
+
+            while (srFile.EndOfStream == false)
+            {
+                string vrstica = srFile.ReadLine();
+                string[] tblStevila = vrstica.Split("\t");
+                int zadnjeStevilo = int.Parse(tblStevila[tblStevila.Length - 1]);
+
+                if (min > zadnjeStevilo)
+                {
+                    min = zadnjeStevilo;
+                }
+
+                List<int> lstVrstica = new List<int>();
+                foreach (string st in tblStevila)
+                {
+                    lstVrstica.Add(int.Parse(st));
+                }
+                lstMatrika.Add(lstVrstica);
+            }
+            srFile.Close();
+
+            StreamWriter sw = new StreamWriter(novaDatoteka);
+
+            foreach (List<int> lstVrstica in lstMatrika)
+            {
+                // Zapis posamezne vrstice
+                for (int i = 0; i < lstVrstica.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        sw.Write((lstVrstica[i] + min) + "\t");
+                    }
+                    else
+                    {
+                        sw.Write(lstVrstica[i] + "\t");
+                    }
+                }
+                sw.WriteLine();
+            }   
+            sw.Close();
+        }
     }
 }
