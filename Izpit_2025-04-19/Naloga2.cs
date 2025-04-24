2db786086fda09af8fee69f901a02695523a2cbf
Borut Lužar <borut.luzar@gmail.com>
2025-04-24 15:21:36 +0200

    Ustni zagovor

--- Izpit_2025-04-19/Naloga2.cs
+++ Izpit_2025-04-19/Naloga2.cs
@@ -37,6 +37,125 @@
         /// </summary>
         public static void ResitevNaloge()
         {
+            /*
+            Prenosnik prenosnik = new Prenosnik(true, "srebrn", 2020);
+            prenosnik.Ima5GModul = true;
+            prenosnik.TipProcesorja = "tip1";
+            */
+
+            Tablica tablica = new Tablica(2023, 30, 7.5);
+            tablica.Ima5GModul = false;
+            tablica.TipProcesorja = "tip2";
+
+            Telefon telefon = new Telefon(true, "Nokia", 2000);
+            telefon.Ima5GModul = true;
+            telefon.TipProcesorja = "tip3";
+
+            Zaposleni z = new Zaposleni();
+            //z.Naprave.Add(prenosnik);
+            z.Naprave.Add(tablica);
+            z.Naprave.Add(telefon);
+
+            Console.WriteLine("Cena najdražje naprave zaposlenega je: " + z.Najdrazja().Cena);
+        }
+
+        /// Napišite razred Zaposleni, 
+        /// ki naj ima lastnosti Naprave (seznam naprav, ki jih ima zaposleni) 
+        /// in SkupnaVrednost (vsota cen vseh naprav zaposlenega). 
+        /// Dodajte še funkcijo, ki vrne najdražjo napravo zaposlenega, 
+        /// in funkcijo, ki vrne najstarejšo napravo zaposlenega.
+        /// 
+        public class Zaposleni
+        {
+
+            public List<MobilnaNaprava> Naprave { get; set; } = new List<MobilnaNaprava>();
+            public MobilnaNaprava SkupnaVrednost { get; set; }
+
+            /// Dodajte še funkcijo, ki vrne najdražjo napravo zaposlenega, 
+            /// in funkcijo, ki vrne najstarejšo napravo zaposlenega.
+            public MobilnaNaprava Najstarejsa()
+            {
+                int najstarejsa = 2025;
+                MobilnaNaprava najstar = Naprave[0];
+                for (int i = 0; i < Naprave.Count(); i++)
+                {
+                    /* */
+                }
+                return najstar;
+
+            }
+
+            public MobilnaNaprava Najdrazja()
+            {
+                return new MobilnaNaprava(2000);
+            }
+        }
+
+        public class MobilnaNaprava
+        {
+            public MobilnaNaprava(int letoIzdelave)
+            {
+                LetoIzdelave = letoIzdelave;
+            }
+            public int LetoIzdelave { get; set; }
+            public int Cena { get; set; }
+            public bool Ima5GModul { get; set; }
+            public string TipProcesorja { get; set; }
+
+            public override string ToString()
+            {
+                string izpis = $"\nLastnosti mobilne naprave: \n" +
+                    $"\nLeto izdelave: {LetoIzdelave}\n" +
+                    $"\nCena: {Cena}\n" +
+                    $"\nIma 5G modul: {(Ima5GModul ? "da" : "ne")}\n" +
+                    $"\nTip procesorja: {TipProcesorja}\n";
+
+                return izpis;
+            }
+        }
+
+        public class Tablica : MobilnaNaprava
+        {
+
+            public Tablica(int letoIzdelave, int casPolnjena, double velikostZaslona) : base(letoIzdelave)
+            {
+                Cena = casPolnjena;
+                VelikostZaslona = velikostZaslona;
+            }
+            public int CasPolnjenja { get; set; }
+            public double VelikostZaslona { get; set; }
+
+            public override string ToString()
+            {
+                string izpis = base.ToString() +
+                    $"Čas polnjenja: {CasPolnjenja}\n" +
+                    $"Velikost zaslona: {VelikostZaslona}\n";
+                return izpis;
+            }
+        }
+        /// <summary>
+        /// telefon, prenosnik
+        /// </summary>
+        public class Telefon : MobilnaNaprava
+        {
+            public Telefon(bool vhodZaSlusalke, string znamka, int letoIzdelave) : base(letoIzdelave)
+            {
+                VhodZaSlusalke = vhodZaSlusalke;
+                Znamka = znamka;
+            }
+            public bool VhodZaSlusalke { get; set; }
+            public string Znamka { get; set; }
+
+            public override string ToString()
+            {
+                string izpis = base.ToString() +
+                    $"\nIma vhod za slusalke: {(VhodZaSlusalke ? "da" : "ne")}\n" +
+                    $"Znamka: {Znamka}\n";
+                return izpis;
+            }
+        }
+        public class Prenosnik
+        {
 
         }
     }
--- Izpit_2025-04-19/Naloga3.cs
+++ Izpit_2025-04-19/Naloga3.cs
@@ -32,7 +32,98 @@
         /// </summary>
         public static void ResitevNaloge()
         {
+            IzpitnaNaloga a = new IzpitnaNaloga(1, 5, 20, "besedilo1");
+            IzpitnaNaloga b = new IzpitnaNaloga(2, 3, 20, "besedilo2");
+            IzpitnaNaloga c = new IzpitnaNaloga(3, 2, 20, "besedilo3");
+            IzpitnaNaloga d = new IzpitnaNaloga(4, 1, 20, "besedilo4");
+            IzpitnaNaloga e = new IzpitnaNaloga(5, 5, 20, "besedilo5");
 
+            Izpit izpit = new Izpit();
+            izpit.Naloge.Add(a);
+            izpit.Naloge.Add(b);
+            izpit.Naloge.Add(c);
+            izpit.Naloge.Add(d);
+            izpit.Naloge.Add(e);
+
+            /// in izpišite podatke o vseh nalogah, pri katerih lahko dosežemo vsaj 10 točk 
+            /// in katerih težavnost je nižja od 4. 
+            
         }
+    }
+
+    public class Izpit
+    {
+        public Izpit(string imePredmeta, string zaporednaStevilka)
+        {
+            ImePredmeta = imePredmeta;
+            ZaporednaStevilka = zaporednaStevilka;
+        }
+
+        public Izpit()
+        {
+            CasPisanja = 90;
+            Naloge = new List<IzpitnaNaloga>();
+        }
+        public string ImePredmeta { get; set; }
+        public string ZaporednaStevilka { get; set; }
+        public DateOnly Datum { get; set; }
+        public int CasPisanja { get; }
+        public List<IzpitnaNaloga> Naloge { get; set; }
+
+        public override string ToString()
+        {
+            string izpis = $"\nPodatki o izpitue: \n" +
+                $"\nIme predmetaa: {ImePredmeta}\n" +
+                $"\nZaporedna Stevilka: {ZaporednaStevilka}\n" +
+                $"\nDatum: {Datum}\n" +
+                $"\nCas Pisanja: {CasPisanja}\n";
+            foreach (IzpitnaNaloga n in Naloge)
+            {
+                izpis += n.ToString();
+            }
+            return izpis;
+        }
+
+        public List<IzpitnaNaloga> VecKot300()
+        {
+            List<IzpitnaNaloga> seznamDolgih = new List<IzpitnaNaloga>();
+            foreach (IzpitnaNaloga n in Naloge)
+            {
+                if (n.Besedilo.Count() >= 300)
+                {
+                    seznamDolgih.Add(n);
+
+                }
+            }
+            return seznamDolgih;
+        }
+    }
+
+    public class IzpitnaNaloga
+    {
+        public int ZaporednaStevilka { get; set; }
+        public int Tezavnost { get; set; }
+        public int SteviloTock { get; set; }
+        public string Besedilo { get; set; }
+
+        public IzpitnaNaloga(int zapSt, int tezavnost, int steviloTock, string besedilo)
+        {
+            ZaporednaStevilka = zapSt;
+            Tezavnost = tezavnost;
+            SteviloTock = steviloTock;
+            Besedilo = besedilo;
+        }
+        public override string ToString()
+        {
+            string izpis = $"\nLastnosti izpitne naloge: \n" +
+                $"\nZaporedna Številka: {ZaporednaStevilka}\n" +
+                $"\nTežavnost: {Tezavnost}\n" +
+                $"\nŠtevilo točk: {SteviloTock}\n" +
+                $"\nBesedilo: {Besedilo}\n";
+            return izpis;
+        }
+
+
+
     }
 }
